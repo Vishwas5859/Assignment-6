93d3034a31fa756c9e1ac766ae71e402dfc661e3
Vishwas5859 <vishwasg370@gmail.com>
2023-07-20 19:35:33 +0530

    Assignment-6

--- new_file.py
+++ new_file.py
@@ -2,9 +2,9 @@ Employees = {'Employee':{'employee1':{"name":"Mike","DOB":"12-01-1985","height":
 "state": "Telangana"},'employee2':{"name":"Raj","DOB":"22-03-1985","height":"1.6m","city":"Bangalore",
 "state": "Karnataka"},'employee3':{"name":"Suraj","DOB":"16-02-1985", "height":"1.55m","city":"Cochi",
 "state": "Kerala"},'employee4':{"name":"Anmol","DOB":"28-07-1985","height":"1.75m","city":"Kolkata",
-"state": "West Bengal"},'employee4':{"name":"Randeep","DOB":"17-04-1985","height":"1.64m","city":"Ahmedabad",
+"state": "West Bengal"},'employee5':{"name":"Randeep","DOB":"17-04-1985","height":"1.64m","city":"Ahmedabad",
 "state": "Gujarat" }}}
 
 import json
-json_obj = json.dumps(Employees,indent=4)
+json_obj = json.dumps(Employees,indent=5)
 print(json_obj)
